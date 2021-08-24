0f0479ec36b9c75ac951d47cb212bca0c7af1074
fuad <fuad.gyulmamedov.2001@mail.ru>
2021-08-24 21:22:32 +0400

    first

---
+++ Game.js
@@ -0,0 +1,68 @@
+class Game {
+    constructor() {}
+
+    player_1 = null
+    player_2 = null
+    player_1_win = 0
+    player_2_win = 0
+    game_list = [null, null, null, null, null, null, null, null, null]
+    now = 'x'
+    all = 0
+
+    set(index) {
+        if (this.game_list[index - 1] == null){
+            this.now == 'o' ? this.now = 'x' : this.now = 'o'
+            this.game_list[index - 1] = this.now
+        }
+    }
+
+    get() {
+        return this.game_list
+    }
+
+    isFinish() {
+        if (!this.game_list.includes(null)) return true
+        else return false
+    }
+
+    setPlayer(name) {
+        if (this.player_1 == null) {
+            this.player_1 = name
+        }
+        else {
+            this.player_2 = name
+        }
+    }
+
+    getPlayer() {
+        return {player_1: this.player_1, player_2: this.player_2}
+    }
+
+    clear() {
+        this.all++
+        this.game_list = [null, null, null, null, null, null, null, null, null]
+    }
+
+    checkWin() {
+        if ((this.game_list[0] == this.now && this.game_list[3] == this.now && this.game_list[6]) == this.now ||
+        (this.game_list[1] == this.now && this.game_list[4] == this.now && this.game_list[7]) == this.now ||
+        (this.game_list[2] == this.now && this.game_list[5] == this.now && this.game_list[8]) == this.now ||
+        (this.game_list[0] == this.now && this.game_list[1] == this.now && this.game_list[2]) == this.now ||
+        (this.game_list[3] == this.now && this.game_list[4] == this.now && this.game_list[5]) == this.now ||
+        (this.game_list[6] == this.now && this.game_list[7] == this.now && this.game_list[8]) == this.now ||
+        (this.game_list[0] == this.now && this.game_list[4] == this.now && this.game_list[8]) == this.now ||
+        (this.game_list[2] == this.now && this.game_list[4] == this.now && this.game_list[6]) == this.now ) {
+            if (this.now == 'x') this.player_1_win++
+            else if (this.now == 'o') this.player_2_win++
+            return true
+        }
+        return false
+    }
+
+    getScore() {
+        return {all: this.all, player_1_win: this.player_1_win, player_2_win: this.player_2_win}
+    }
+
+}
+
+module.exports = Game
---
+++ server.js
@@ -0,0 +1,51 @@
+let express = require('express')
+const Game = require('../game/Game')
+let app = express()
+let http = require('http').createServer(app)
+let io = require('socket.io')(http,{
+    cors: {
+      origin: '*',
+    }
+  })
+
+let mainArray = {}
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+io.on('connection', (socket) => {
+    socket.join('test')
+    mainArray['test'] = new Game()
+
+    socket.on('save_player', data => {
+        mainArray['test'].setPlayer(data.name)
+    })
+
+    socket.on('get_player', () => {
+        io.in('test').emit('get_player', mainArray['test'].getPlayer())
+    })
+
+    socket.on('send_message', data => {
+        io.in('test').emit('send_message', data)
+    })
+
+    socket.on('set_position', (data) => {
+        mainArray['test'].set(data.index)
+        if (mainArray['test'].checkWin()) {
+            mainArray['test'].clear()
+            io.in('test').emit('get_score', mainArray['test'].getScore())
+        }
+        if (mainArray['test'].isFinish()) {
+            mainArray['test'].clear()
+            io.in('test').emit('get_score', mainArray['test'].getScore())
+        }
+        io.in('test').emit('set_position', mainArray['test'].get())
+    })
+})
+
+let PORT = process.env.PORT || 8000
+
+http.listen(PORT, () => {
+    console.log(`Started on port: ${PORT}`);
+})
